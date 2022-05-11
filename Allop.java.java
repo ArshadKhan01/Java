a94facdd0f08bf6120c9f8de93edbe402fe02fc4
Arshad Khan <105267942+ArshadKhan01@users.noreply.github.com>
2022-05-11 22:55:49 +0530

    Add files via upload

---
+++ Allop.java.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Allop;
+
+/**
+ *
+ * @author Arshad Khan
+ */
+public class java {
+    public static void main(String args[])
+    {
+        int a,b;
+        a=8;
+        b=7;
+        
+        System.out.println("addition="+(a+b));
+        System.out.println("subtraction="+(a-b));
+        System.out.println("multiplication="+(a*b));
+        System.out.println("division="+(a/b));
+        System.out.println("per="+(a%b));
+    }}
